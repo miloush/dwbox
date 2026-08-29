03c25e35e63fab4c41a62efc74c32d02a16045e6
Jan Kučera <miloush@users.noreply.github.com>
2026-08-29 10:48:59 +0100

    external tooltip

---
+++ DWBox/Controls/FontFaceInfo.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace DWBox
+{
+    public partial class FontFaceInfo : UserControl
+    {
+        public FontFaceInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
